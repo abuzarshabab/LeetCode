486c88adf67525a65908ced3e3ad3c91a2c8cc6b
abuzarshabab <abuzarshabab@gmail.com>
2022-10-31 16:57:46 +0530

    Updated shuffle array code

--- string/ShuffleArray.js
+++ string/ShuffleArray.js
@@ -4,9 +4,13 @@
  * @return {string}
  */
  var restoreString = function(s, indices) {
-  let shuffledString = [];
+  let shuffledArray = [];
+  let shuffledString = ''
   for(let i = 0; i < indices.length; i++) {
-      shuffledString[indices[i]] = s[i]
-  }    
+      shuffledArray[indices[i]] = s[i]
+  }   
+  shuffledArray.forEach( char => {
+      shuffledString += char
+  })
   return shuffledString
 };
